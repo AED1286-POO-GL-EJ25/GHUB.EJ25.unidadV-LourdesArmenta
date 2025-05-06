af05e5115dc782dbfde4cde6560e815909f0b4a0
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-05-06 16:58:07 +0000

    .

--- miPrincipal/TestException.java
+++ miPrincipal/TestException.java
@@ -4,19 +4,25 @@ import java.util.Scanner;
 public class TestException{
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        try{
-            System.out.print("Introduce un valor entero :");
-            int num1= entrada.nextInt();
-            System.out.print("Introduce un valor entero diferente de cero:");
-            int num2= entrada.nextInt();
-            int resultado = num1/num2;
-            System.out.println("El resultado = "+resultado);
-        }catch (ArithmeticException ex){
-            System.out.println("ocurrio un error de division por cero:"+ex.getMessage());
-        }catch(InputMismatchException ex){
-            System.out.println("introduciste un valor invalido: "+ex.getMessage());
+        while (true) {
+            try{
+                System.out.print("Introduce un valor entero :");
+                int num1= entrada.nextInt();
+                System.out.print("Introduce un valor entero diferente de cero:");
+                int num2= entrada.nextInt();
+                int resultado = num1/num2;
+                System.out.println("El resultado = "+resultado);
+                break;
+            }catch (ArithmeticException ex){
+                System.out.println("\\u001B[31m\\t***Error:ocurrio un error de division por cero.***\u001B[0m");
+                entrada.nextLine();
+            }catch(InputMismatchException ex){
+                System.out.println("\u001B[31m\t***Error: Debe ingresar un número entero válido.***\u001B[0m");
+                entrada.nextLine(); // Consumir la nueva línea
 
+            }
         }
+        System.out.println("Lo que sigue....");
         
         
     }
