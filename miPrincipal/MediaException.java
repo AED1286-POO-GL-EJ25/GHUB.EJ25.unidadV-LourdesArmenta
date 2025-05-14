502f32044b212cef13ae5061268a17141af67845
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-05-14 16:38:50 +0000

    .

---
+++ miPrincipal/MediaException.java
@@ -0,0 +1,11 @@
+package miPrincipal;
+
+public class MediaException extends Exception{
+    public MediaException(){
+        super();
+    }
+    public MediaException(String mensaje){
+        super(mensaje);
+    }
+    
+}
--- miPrincipal/TestCalcularMediaException.java
+++ miPrincipal/TestCalcularMediaException.java
@@ -1,26 +1,28 @@
 package miPrincipal;
 
 public class TestCalcularMediaException {
-     public static void main(String[] args) {
-        System.out.println("La media es:"+calcularMedia(7));
+     public static void main(String[] args) throws MediaException {
+        System.out.println("La media es:"+calcularMedia(10));
         
     }
-    public static double calcularMedia(int num)  {
+    public static double calcularMedia(int num) throws MediaException {
         double c[]={1.2,2.2,3.3,4.4,5.5,6.6,7.7};
         double media;
-        
-        
+        try{
             media = avg(c,num);
             return media;
-       
-            
-
-
+        }catch(MediaException ex){
+            System.out.println("Excepcion captada:"+ex);
+            System.out.println("para calcular la media utilice el tamaño del arreglo que es de: "+c.length);
+            num = c.length;
+            return avg(c,num);
         }
-        
+       
 
     }
-    public static double avg(double []b,int num) {
+    public static double avg(double []b,int num) throws MediaException{
+        if(num>b.length)
+            throw new MediaException("n es mas grande que el tamaño del arreglo");
        
         double media =0;
         for(int i=0;i<num;i++){
